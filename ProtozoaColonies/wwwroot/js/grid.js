17cc727efbac738f38996e4d50c64e8391975645
Joshua <64.josh@gmail.com>
2020-02-27 20:20:08 -0700

    Added buttons, reduced grid to 30x30

---
+++ ProtozoaColonies/wwwroot/js/grid.js
@@ -0,0 +1,76 @@
+﻿//Functions triggered on page load:
+$(function () {
+
+    // Buttons:
+    $('#btnToggleStatus').click(function () {
+        if ($(this).html() == 'Start') {
+            $(this).html('Stop');
+            // Add start logic here
+        }
+        else {
+            $(this).html('Start');
+            // Add stop logic here
+        }
+    });
+    $('#btnNext').click(function () {
+        // Add next logic here
+    });
+
+    // Generate Grid:
+    $("#tableContainer").append(generateGrid(30, 30));
+
+    // Handle clicks/mouse drag on grid:
+    var isMouseDown = false;
+    $("td")
+        .mousedown(function () {
+            isMouseDown = true;
+            $(this).toggleClass("highlighted");
+
+            var index = $("td").index(this);
+            var row = Math.floor((index) / 30);
+            var col = (index % 30);
+            console.log("That was row " + row + " and col " + col);
+
+            return false; // prevent text selection
+        })
+        .mouseover(function () {
+            if (isMouseDown) {
+                $(this).toggleClass("highlighted");
+
+                var index = $("td").index(this);
+                var row = Math.floor((index) / 30);
+                var col = (index % 30);
+                console.log("That was row " + row + " and col " + col);
+            }
+        })
+        .bind("selectstart", function () {
+            return false; // prevent text selection in IE
+        });
+
+    $(document)
+        .mouseup(function () {
+            isMouseDown = false;
+        });
+});
+
+// Draw grid:
+function generateGrid(rows, cols) {
+    var grid = "<table>";
+    for (row = 1; row <= rows; row++) {
+        grid += "<tr>";
+        for (col = 1; col <= cols; col++) {
+            grid += "<td></td>";
+        }
+        grid += "</tr>";
+    }
+    return grid;
+}
+
+// Color Picker:
+var playerColor = "#BF2828"
+function changeColor() {
+    playerColor = document.getElementById("colorPicker").value;
+    console.log(playerColor);
+    document.documentElement.style
+        .setProperty('--player-color', playerColor);
+}
--- ProtozoaColonies/wwwroot/js/site.js
+++ ProtozoaColonies/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿//Functions triggered on page load:
 $(function () {
 
-    // Title Animations:
+    // Title Animations: (UNCOMMENT FOR RELEASE)
     //$('#intro0').fadeIn(1500, function () {
     //    $('#intro1').fadeIn(1500, function () {
     //        $('#intro2').fadeIn(1500, function () {
@@ -15,62 +15,4 @@ $(function () {
     //        });
     //    });
     //});
-
-    // Generate Grid:
-    $("#tableContainer").append(generateGrid(50, 50));
-
-    // Handle clicks/mouse drag:
-    var isMouseDown = false;
-    $("td")
-        .mousedown(function () {
-            isMouseDown = true;
-            $(this).toggleClass("highlighted");
-
-            var index = $("td").index(this);
-            var row = Math.floor((index) / 50);
-            var col = (index % 50);
-            console.log("That was row " + row + " and col " + col);
-
-            return false; // prevent text selection
-        })
-        .mouseover(function () {
-            if (isMouseDown) {
-                $(this).toggleClass("highlighted");
-
-                var index = $("td").index(this);
-                var row = Math.floor((index) / 50);
-                var col = (index % 50);
-                console.log("That was row " + row + " and col " + col);
-            }
-        })
-        .bind("selectstart", function () {
-            return false; // prevent text selection in IE
-        });
-
-    $(document)
-        .mouseup(function () {
-            isMouseDown = false;
-        });
 });
-
-// Draw grid:
-function generateGrid(rows, cols) {
-    var grid = "<table>";
-    for (row = 1; row <= rows; row++) {
-        grid += "<tr>";
-        for (col = 1; col <= cols; col++) {
-            grid += "<td></td>";
-        }
-        grid += "</tr>";
-    }
-    return grid;
-}
-
-// Color Picker:
-var playerColor = "#bf2828"
-function changeColor() {
-    playerColor = document.getElementById("colorPicker").value;
-    console.log(playerColor);
-    document.documentElement.style
-        .setProperty('--player-color', playerColor);
-}
