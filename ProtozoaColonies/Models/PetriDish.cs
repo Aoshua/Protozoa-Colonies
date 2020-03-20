375bc0cb3a6b53f82562fa81c3a1f96fcb143e48
Joshua <64.josh@gmail.com>
2020-03-19 21:09:58 -0600

    Mostly functional...

--- ProtozoaColonies/Models/PetriDish.cs
+++ ProtozoaColonies/Models/PetriDish.cs
@@ -194,7 +194,7 @@ namespace ProtozoaColonies.Models
 			{
 				if (!(cell.x == 0 || cell.x == (size - 1) || cell.y == 0 || cell.y == (size - 1)))
 				{
-					sharedDish.Add(new Cell((cell.x - 1), (cell.y - 1), cell.color));
+					sharedDish.Add(new Cell((cell.x), (cell.y), cell.color));
 				}
 			}
 
